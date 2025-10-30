c73fba5c7a6a36340c0e66dc06f5187ac7c28fd9
user <user@users-MacBook-Pro.local>
2025-10-30 12:58:47 +0700

    remove vercel config and add railway config

--- api/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-@mkdir(__DIR__ . '/../storage/framework/views', 0777, true);
-@mkdir(__DIR__ . '/../storage/framework/cache', 0777, true);
-@mkdir(__DIR__ . '/../storage/logs', 0777, true);
--- api/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-require __DIR__ . '/bootstrap.php';
-require __DIR__ . '/../public/index.php';
