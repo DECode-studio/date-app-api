6b6fde7dcadb2dccdc86e20a4053c9f63354041c
user <user@users-MacBook-Pro.local>
2025-10-30 12:40:17 +0700

    add vercel config

---
+++ api/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+@mkdir(__DIR__ . '/../storage/framework/views', 0777, true);
+@mkdir(__DIR__ . '/../storage/framework/cache', 0777, true);
+@mkdir(__DIR__ . '/../storage/logs', 0777, true);
